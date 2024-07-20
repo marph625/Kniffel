e3cf13054fc440dbae922eb3a2427231efedbd61
marph625 <marvin.gnesner@gmail.com>
2024-07-20 02:02:35 +0200

    switching elements between array lists

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,80 @@ import java.util.Scanner;
 
 public class Main {
 
-    static ArrayList eyes = new ArrayList();
-    static ArrayList kept = new ArrayList();
-    static ArrayList rerolled = new ArrayList();
+    static ArrayList<Integer> eyes = new ArrayList();
+    static ArrayList<Integer> kept = new ArrayList();
+    static ArrayList<Integer> rerolled = new ArrayList();
     static Scanner sc = new Scanner(System.in);
     static Dice d1 = new Dice();
     static boolean isHappy;
 
     public static void main(String[] args) {
-//        playerRound();
-//        System.out.println("Behalten: " + kept);
-//        System.out.println("Neu würfeln: " + rerolled);
-//        playerRound();
+        System.out.println("###################################");
+        System.out.println("############# KNIFFEL #############");
+        System.out.println("###################################");
+        firstRound();
+
+        secondRound(rerolled);
 //        System.out.println("Behalten: " + kept);
 //        System.out.println("Neu würfeln: " + rerolled);
 
-        System.out.println(firstRoll());
-        System.out.println(reroll(eyes));
+//        System.out.println(firstRoll());
+//        System.out.println(reroll(eyes));
     }
 
+    private static void secondRound(ArrayList<Integer> al) {
+
+
+        for (int round = 1; round < 3; round++) {
+            isHappy = false;
+            eyes = reroll(al);
+
+            System.out.println("Neu gewürfelt: " + round);
+            System.out.println("Behalten: " + kept);
+            System.out.println("Gewürfelt: " + eyes);
+
+            while (!isHappy) {
+                System.out.println("Welche Würfel behalten?\nGib 5 ein, wenn du zufrieden bist.");
+
+                int input = sc.nextInt();
+
+                if (input == 5) {
+                    isHappy = true;
+                } else {
+                    kept.add(eyes.get(input));
+                    eyes.remove(eyes.get(input));
+                    rerolled = eyes;
+                    System.out.println("Behalten: " + kept);
+                    System.out.println("Neu würfeln: " + rerolled);
+                }
+            }
+        }
+    }
+
+
+
+    public static void firstRound() {
+        isHappy = false;
+
+        firstRoll();
+
+        System.out.println("Gewürfelt: " + eyes);
+
+        while (!isHappy) {
+            System.out.println("Welche Würfel behalten?\nGib 5 ein, wenn du zufrieden bist.");
+            int input = sc.nextInt();
+
+            if (input == 5) {
+                isHappy = true;
+            } else {
+                kept.add(eyes.get(input));
+                eyes.remove(eyes.get(input));
+                rerolled = eyes;
+                System.out.println("Behalten: " + kept);
+                System.out.println("Neu würfeln: " + rerolled);
+            }
+        }
+    }
     public static ArrayList<Integer> firstRoll() {
         while (eyes.size() < 5) {
             int result = d1.roll();
@@ -31,37 +86,12 @@ public class Main {
     }
 
     public static ArrayList<Integer> reroll(ArrayList al) {
+        int elements = al.size();
         al.clear();
-        while (al.size() < 5) {
+        while (al.size() < elements) {
             int result = d1.roll();
             if (result != 0) al.add(result);
         }
         return al;
     }
-
-    public static void playerRound() {
-        isHappy = false;
-
-        firstRoll();
-
-        System.out.println("Gewürfelt: " + eyes);
-
-        while (!isHappy) {
-            System.out.println("Welche Würfel behalten? Gib 5 ein, wenn du zufrieden bist.");
-            int input = sc.nextInt();
-
-            if (input == 5) {
-                isHappy = true;
-            } else {
-                kept.add(eyes.get(input));
-                eyes.remove(eyes.get(input));
-                rerolled = eyes;
-                reroll(rerolled);
-                System.out.println("Behalten: " + kept);
-                System.out.println("Neu würfeln: " + rerolled);
-            }
-        }
-
-    }
-
 }
